9f9bd1a383328aa72656a21609a14a213a8f8e3e
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2023-11-04 22:20:50 -0300

    added queryparams for front

--- api/src/main/java/com/api/controllers/EmployeeController.java
+++ api/src/main/java/com/api/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.api.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.models.Employee;
@@ -24,7 +25,7 @@ public class EmployeeController {
 					@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Employee.class))) }),
 			@ApiResponse(responseCode = "201", description = "No hay empleados registrados", content = @Content)})
 	@GetMapping("/")
-	public ArrayList<Employee> getAll() {
+	public ArrayList<Employee> getAll(@RequestParam(required = false) String type, @RequestParam(required = false) String compensationGrade) {
 		ArrayList<Employee> res = new ArrayList<>();
 		res.add(new Employee());
 		return res;
