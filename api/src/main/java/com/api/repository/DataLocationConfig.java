7b9e33c75b07fbfc511d39528e541769d13c7cb6
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2023-11-05 19:42:59 -0300

    added by mistake

--- api/src/main/java/com/api/repository/DataLocationConfig.java
+++
@@ -1,8 +0,0 @@
-package com.api.repository;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("file-locations")
-public record DataLocationConfig(String compensation, String employee) {
-
-}
