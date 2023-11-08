9c1ad62dc808890182532d939ffcc2dfb3f15ae0
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2023-11-08 15:56:06 -0300

    repo config and definition

--- api/src/main/java/com/api/main/App.java
+++ api/src/main/java/com/api/main/App.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.api.controllers")
-@EntityScan(basePackages = "com.api.models")
+@ComponentScan(basePackages = "com.api")
+@EnableJpaRepositories(basePackages = "com.api.repository")
+@EntityScan(basePackages = "com.api.repository.entities.entityImplementations")
 public class App {
 
 
--- api/src/main/java/com/api/repository/IEmployeeRepository.java
+++ api/src/main/java/com/api/repository/IEmployeeRepository.java
@@ -1,13 +1,27 @@
 package com.api.repository;
 
-import com.api.models.entities.EmployeeEntity;
-
 import java.util.Map;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import com.api.repository.entities.entityImplementations.EmployeeEntity;
 
 @Repository
 public interface IEmployeeRepository extends ListCrudRepository<EmployeeEntity, String> {
 
-    public Map<EmployeeEntity, EmployeeEntity> getEmployeeByManager(String managerId);
+    @Query(
+        value = "Select * FROM EMPLOYEES e WHERE e.manager_employee_id = :managerId",
+        nativeQuery = true
+    )
+    public Map<String, EmployeeEntity> findByManager(@Param("managerId")String managerId);
+
+    @Query(
+        value = "Select * FROM EMPLOYEES e WHERE e.matrix_manager_employee_id = :matrixManagerId",
+        nativeQuery = true
+    )
+    public Map<String, EmployeeEntity> findByMatrixManager(@Param("matrixManagerId")String matrixManagerId);
 }
