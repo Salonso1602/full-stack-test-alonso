c4bbb8d0fb7c103252b969203df06512dd988cee
SAlonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2023-11-09 01:20:12 -0300

    configs and imports

--- consultingWeb/src/app/app-routing.module.ts
+++ consultingWeb/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule) },
+  { path: 'logIn', loadChildren: () => import('./pages/login-page/login-page.module').then(m => m.LoginPageModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
